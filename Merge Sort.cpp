ccbc7ae051f6588388dc70333c6e1a2b67bca2f0
sami <Mohammadsami78608@gmail.com>
2021-03-15 12:44:10 +0530

    hi

---
+++ Merge Sort.cpp
@@ -0,0 +1,10 @@
+/ C++ program for implementation of Bubble sort 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+void swap(int *xp, int *yp) 
+{ 
+	int temp = *xp; 
+	*xp = *yp; 
+	*yp = temp; 
+} 
